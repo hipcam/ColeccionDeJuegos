77f19689ad267dfc4e7ccdb3f3b721884a8e320a
Fernanda <alessandrafernanda.alvarado@tecsup.edu.pe>
2018-09-25 16:46:26 -0500

    2 commit

---
+++ ColeccionDeJuegos/JuegoViewController.swift
@@ -0,0 +1,60 @@
+//
+//  JuegoViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Fernanda  on 21/09/18.
+//  Copyright © 2018 Fernanda Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class JuegoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var tituloTextField: UITextField!
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imagenSeleccionada = info[UIImagePickerControllerOriginalImage] as! UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context:context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = UIImagePNGRepresentation(JuegoImageView.image!) 
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
